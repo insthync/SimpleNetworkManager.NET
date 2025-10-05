282bd6223709ae7894b49d86b2c033dd064e50ae
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2025-10-06 02:04:04 +0700

    feat: add stream extensions tests and implement stream write/read functions testing

---
+++ SimpleNetworkManager.NET/SimpleNetworkManager.NET.Tests/Network/StreamExtensionsTests.cs
@@ -0,0 +1,38 @@
+﻿using Insthync.SimpleNetworkManager.NET.Messages;
+using Insthync.SimpleNetworkManager.NET.Network;
+using Insthync.SimpleNetworkManager.NET.Tests.Messages;
+using MessagePack;
+
+namespace Insthync.SimpleNetworkManager.NET.Tests.Network
+{
+    public class StreamExtensionsTests
+    {
+        [Fact]
+        public async Task ReadMessage_Test()
+        {
+            using var stream = new MemoryStream();
+            var originalMessage = new TestMessage()
+            {
+                intVal = 999,
+                boolVal = true,
+                stringVal = "Test",
+            };
+            var cancellationTokenSource = new CancellationTokenSource();
+            await stream.WriteMessageAsync(originalMessage, cancellationTokenSource.Token);
+            stream.Position = 0;
+            var message = await stream.ReadMessageAsync(cancellationTokenSource.Token);
+            Assert.NotNull(message);
+
+            var messageData = BaseMessage.ExtractMessageData(message, out var messageType);
+            Assert.Equal(originalMessage.GetMessageType(), messageType);
+
+            // Deserialize the message data using MessagePack
+            TestMessage? deserializedMessage = (TestMessage?)MessagePackSerializer.Deserialize(originalMessage.GetType(), messageData, originalMessage.GetMessagePackOptions());
+
+            // Assert
+            Assert.Equal(originalMessage.intVal, deserializedMessage?.intVal);
+            Assert.Equal(originalMessage.boolVal, deserializedMessage?.boolVal);
+            Assert.Equal(originalMessage.stringVal, deserializedMessage?.stringVal);
+        }
+    }
+}
