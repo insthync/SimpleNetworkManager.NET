d629a1ffbbd969c88e77545b9d0765ea165d7f38
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2025-10-05 22:49:20 +0700

    feat: add message handler

---
+++ SimpleNetworkManager.NET/SimpleNetworkManager.NET/Messages/BaseMessageHandler.cs
@@ -0,0 +1,27 @@
+﻿using Cysharp.Threading.Tasks;
+using System;
+
+namespace Insthync.SimpleNetworkManager.NET.Messages
+{
+    public abstract class BaseMessageHandler<T> : IMessageHandler
+        where T : BaseMessage
+    {
+        private static T? s_messageInstance;
+
+        public virtual BaseMessage GetMessageInstance()
+        {
+            if (s_messageInstance == null)
+                s_messageInstance = Activator.CreateInstance<T>();
+            return s_messageInstance;
+        }
+
+        public UniTask HandleDataAsync(object? data)
+        {
+            if (data == null)
+                throw new ArgumentNullException(nameof(data));
+            return HandleAsync((T)data);
+        }
+
+        protected abstract UniTask HandleAsync(T data);
+    }
+}
---
+++ SimpleNetworkManager.NET/SimpleNetworkManager.NET/Messages/IMessageHandler.cs
@@ -0,0 +1,10 @@
+﻿using Cysharp.Threading.Tasks;
+
+namespace Insthync.SimpleNetworkManager.NET.Messages
+{
+    public interface IMessageHandler
+    {
+        BaseMessage GetMessageInstance();
+        public UniTask HandleDataAsync(object? data);
+    }
+}
