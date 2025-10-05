7aa6f4d3a3c4ad8d62997c43782a50eafcb6cc96
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2025-10-06 01:20:36 +0700

    feat: message serialize/deserialize testing

---
+++ SimpleNetworkManager.NET/SimpleNetworkManager.NET.Tests/Messages/MessageTests.cs
@@ -0,0 +1,34 @@
+﻿using Insthync.SimpleNetworkManager.NET.Messages;
+using MessagePack;
+
+namespace Insthync.SimpleNetworkManager.NET.Tests.Messages
+{
+    public class MessageTests
+    {
+        [Fact]
+        public void TestMessage_Serialize_Deserialize()
+        {
+            var originalMessage = new TestMessage()
+            {
+                intVal = 123,
+                boolVal = true,
+                stringVal = "Hello",
+            };
+
+            // Act - Serialize using custom binary format
+            var serializedData = originalMessage.Serialize();
+
+            // Extract message data and verify header
+            var messageData = BaseMessage.ExtractMessageData(serializedData, out uint messageType);
+            Assert.Equal(originalMessage.GetMessageType(), messageType);
+
+            // Deserialize the message data using MessagePack
+            TestMessage? deserializedMessage = (TestMessage?)MessagePackSerializer.Deserialize(originalMessage.GetType(), messageData, originalMessage.GetMessagePackOptions());
+
+            // Assert
+            Assert.Equal(originalMessage.intVal, deserializedMessage?.intVal);
+            Assert.Equal(originalMessage.boolVal, deserializedMessage?.boolVal);
+            Assert.Equal(originalMessage.stringVal, deserializedMessage?.stringVal);
+        }
+    }
+}
---
+++ SimpleNetworkManager.NET/SimpleNetworkManager.NET.Tests/Messages/TestMessage.cs
@@ -0,0 +1,23 @@
+﻿using Insthync.SimpleNetworkManager.NET.Messages;
+using MessagePack;
+
+namespace Insthync.SimpleNetworkManager.NET.Tests.Messages
+{
+    [MessagePackObject]
+    public class TestMessage : BaseMessage
+    {
+        public override uint GetMessageType()
+        {
+            return 1;
+        }
+
+        [Key(0)]
+        public int intVal;
+
+        [Key(1)]
+        public bool boolVal;
+
+        [Key(2)]
+        public string? stringVal;
+    }
+}
