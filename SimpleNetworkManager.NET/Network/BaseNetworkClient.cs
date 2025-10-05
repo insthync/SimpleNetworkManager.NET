7cc399351c435a6f2cac1200be1be5897953ec40
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2025-10-06 02:58:10 +0700

    feat: add network client abstract class

---
+++ SimpleNetworkManager.NET/Network/BaseNetworkClient.cs
@@ -0,0 +1,27 @@
+﻿using Cysharp.Threading.Tasks;
+using Insthync.SimpleNetworkManager.NET.Services;
+using Microsoft.Extensions.Logging;
+
+namespace Insthync.SimpleNetworkManager.NET.Network
+{
+    public abstract class BaseNetworkClient
+    {
+        protected readonly ILoggerFactory _loggerFactory;
+        protected readonly ILogger<BaseNetworkClient> _logger;
+        protected readonly MessageRouter _messageRouter;
+
+        public MessageRouter MessageRouter => _messageRouter;
+        public abstract BaseClientConnection? ClientConnection { get; }
+        public bool IsConnected => ClientConnection?.IsConnected ?? false;
+
+        public BaseNetworkClient(ILoggerFactory loggerFactory)
+        {
+            _loggerFactory = loggerFactory;
+            _logger = _loggerFactory.CreateLogger<BaseNetworkClient>();
+            _messageRouter = new MessageRouter(_loggerFactory.CreateLogger<MessageRouter>());
+        }
+
+        public abstract UniTask ConnectAsync(string hostname, int port);
+        public abstract UniTask DisconnectAsync();
+    }
+}
