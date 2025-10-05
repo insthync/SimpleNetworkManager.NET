1a8effba6134703bead4ea0116db97292a2f5092
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2025-10-05 22:50:05 +0700

    feat: add message router, it will collects message handlers by message type

---
+++ SimpleNetworkManager.NET/SimpleNetworkManager.NET/Services/MessageRouter.cs
@@ -0,0 +1,78 @@
+﻿using Cysharp.Threading.Tasks;
+using Insthync.SimpleNetworkManager.NET.Messages;
+using Insthync.SimpleNetworkManager.NET.Network;
+using MessagePack;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Concurrent;
+
+namespace Insthync.SimpleNetworkManager.NET.Services
+{
+    public class MessageRouter
+    {
+        private ILogger<MessageRouter> _logger;
+        private readonly ConcurrentDictionary<uint, IMessageHandler> _handlers;
+
+        public MessageRouter(ILogger<MessageRouter> logger)
+        {
+            _logger = logger;
+            _handlers = new ConcurrentDictionary<uint, IMessageHandler>();
+        }
+
+        /// <summary>
+        /// Registers a message handler for a specific message type
+        /// </summary>
+        /// <typeparam name="T">Type of message the handler processes</typeparam>
+        /// <param name="handler">Handler instance to register</param>
+        /// <exception cref="ArgumentNullException">Thrown when handler is null</exception>
+        public void RegisterHandler<T>(BaseMessageHandler<T> handler)
+            where T : BaseMessage
+        {
+            if (handler == null)
+                throw new ArgumentNullException(nameof(handler));
+
+            // Get the message type from the generic type parameter
+            var messageInstance = handler.GetMessageInstance();
+            var messageType = messageInstance.GetMessageType();
+
+            if (_handlers.TryAdd(messageType, handler))
+            {
+                _logger.LogDebug("Registered handler for message type {MessageType} ({TypeName})",
+                    messageType, typeof(T).Name);
+            }
+            else
+            {
+                _logger.LogWarning("Handler for message type {MessageType} ({TypeName}) already registered, replacing",
+                    messageType, typeof(T).Name);
+                _handlers[messageType] = handler;
+            }
+        }
+
+        /// <summary>
+        /// Routes a message to the appropriate handler
+        /// </summary>
+        /// <param name="clientConnection">Client connection that sent the message</param>
+        /// <param name="message">Message to route</param>
+        /// <returns>Task representing the async routing operation</returns>
+        /// <exception cref="ArgumentNullException">Thrown when client or message is null</exception>
+        public async UniTask RouteMessageAsync(BaseClientConnection clientConnection, byte[] message)
+        {
+            if (clientConnection == null)
+                throw new ArgumentNullException(nameof(clientConnection));
+            if (message == null)
+                throw new ArgumentNullException(nameof(message));
+
+            var data = BaseMessage.ExtractMessageData(message, out var messageType);
+            if (!_handlers.TryGetValue(messageType, out var handler))
+            {
+                _logger.LogWarning("No handler registered for message type {MessageType} from client {ConnectionId}",
+                    messageType, clientConnection.ConnectionId);
+                return;
+            }
+
+            var messageInstance = handler.GetMessageInstance();
+            var dataType = messageInstance.GetType();
+            await handler.HandleDataAsync(MessagePackSerializer.Deserialize(messageInstance.GetType(), data, messageInstance.GetMessagePackOptions()));
+        }
+    }
+}
